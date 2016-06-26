ba467a9b46ad6cc562d9e1770ae17653583d167b
Tony <terimyaki@users.noreply.github.com>
2016-06-26 19:09:06 -0400

    log

--- sensors-reader/modules/calculator.js
+++ sensors-reader/modules/calculator.js
@@ -5,7 +5,7 @@ const MAX_SALINITY = 100000;
 function voltageToPpm(millivolts){
     const resistance = (10000 / (millivolts / 1000)) - 2000,
         salinity = 257396 * Math.pow(resistance, -1.72);
-
+    console.log('millivolts', millivolts, 'resistance', resistance, 'salinity', salinity);
 	// TODO: Need to test in order to establish constants for the function of resistance to tds
 
     // if salinity is Infinite, set it to a really high number
