21a8e8c23e50a2e1eb9eebe7736888727aa5f12b
Tony <terimyaki@users.noreply.github.com>
2016-08-13 16:12:48 -0400

    added read poll all test

--- demo/send-test.js
+++ demo/send-test.js
@@ -1,6 +1,6 @@
 'use strict';
-const home = require('./modules/home-communication'),
-    formatters = require('./formatters');
+const home = require('../modules/home-communication'),
+    formatters = require('../formatters');
 
 function randomInt(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -12,10 +12,13 @@ function generateData(){
         ppmOut : randomInt(1000, 2000),
         ppmIn : randomInt(3500, 4500),
         ppmRec : randomInt(1000, 2000),
+        // ppmOut : randomInt(5000, 12000),
+        // ppmIn : randomInt(6500, 9500),
+        // ppmRec : randomInt(2000, 4000),
         date: new Date() 
     },
     formattedData = formatters.toFirebase(data);
-
+    console.log('formatted', formattedData);
     return formattedData;
 }
 
