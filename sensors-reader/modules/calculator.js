7a69b25180eb5fb55f7d38a7e504effb7140c4f8
Jesse <mienaikoe@gmail.com>
2016-08-14 17:21:23 -0400

    Update calculator.js

--- sensors-reader/modules/calculator.js
+++ sensors-reader/modules/calculator.js
@@ -1,6 +1,10 @@
 'use strict';
 
 const MAX_SALINITY = 100000;
+const A_N = 11483;
+const B_N = -1.7373;
+// These are calculated from a power law least squares fit on actual readings of known salinity;
+// http://mathworld.wolfram.com/LeastSquaresFittingPowerLaw.html
 
 function voltageToPpm(millivolts){
     const resistance = ((10000 / (millivolts / 1000)) - 2000) / 1000,
