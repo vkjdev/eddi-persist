b50d9e323761d25140dcdd13c768327ec1b16ad7
Jesse <mienaikoe@gmail.com>
2016-08-14 17:13:01 -0400

    Update calculator.js

--- sensors-reader/modules/calculator.js
+++ sensors-reader/modules/calculator.js
@@ -1,12 +1,18 @@
 'use strict';
 
+const A_N = 11483;
+const B_N = -1.7373;
+// calculated via an inverse of the Power Law of Least Squares Fitting
+// http://mathworld.wolfram.com/LeastSquaresFittingPowerLaw.html
+
 function voltageToPpm(millivolts){
     const salResistorOhms = 1000000,
         ohms = (millivolts / (5000.0 - millivolts)) * salResistorOhms;
 
 	// TODO: Need to test in order to establish constants for the function of resistance to tds
+    
 
-	return ohms / 100;
+    return A_N * Math.pow(ohms, B_N);
 }
 
 function rawToMillivolts(raw){
